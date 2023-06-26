fd3a00355772013c5caa77760d160ebc90f6c081
syb8200 <syb8200@naver.com>
2023-06-27 01:01:45 +0900

    dataBinding 연결 및 ui 작성

--- app/src/main/java/com/practice/fc_3_chapter5/MainActivity.kt
+++ app/src/main/java/com/practice/fc_3_chapter5/MainActivity.kt
@@ -2,10 +2,40 @@ package com.practice.fc_3_chapter5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+import com.practice.fc_3_chapter5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            view = this@MainActivity
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    // 실제 입력 후에, 입력 완료를 눌렀을 때 호출
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    // text가 변경될 때마다 호출
+                    return false
+                }
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
