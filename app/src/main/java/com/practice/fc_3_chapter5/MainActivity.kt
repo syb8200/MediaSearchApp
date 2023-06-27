ec54b219e8e997428faa67fa85a350c36800333e
syb8200 <syb8200@naver.com>
2023-06-28 02:25:13 +0900

    RxJava 사용하여 debounce 처리

--- app/src/main/java/com/practice/fc_3_chapter5/MainActivity.kt
+++ app/src/main/java/com/practice/fc_3_chapter5/MainActivity.kt
@@ -6,6 +6,12 @@ import android.view.Menu
 import android.widget.SearchView
 import com.google.android.material.tabs.TabLayoutMediator
 import com.practice.fc_3_chapter5.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -13,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     private val searchFragment = SearchFragment()
     private val fragmentList = listOf(searchFragment, FavoriteFragment())
     private val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle, fragmentList)
+
+    // Rx는 activity 해제될 때 같이 해제시켜줘야 하기 때문에 nullable
+    private var observableTextQuery : Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,23 +46,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        observableTextQuery?.dispose()
+        observableTextQuery = null
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
 
-        (menu.findItem(R.id.search).actionView as SearchView).apply {
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    // 실제 입력 후에, 입력 완료를 눌렀을 때 호출
-                    return false
-                }
+        observableTextQuery = Observable.create { emitter : ObservableEmitter<String>? ->
+            (menu.findItem(R.id.search).actionView as SearchView).apply {
+                setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        // 실제 입력 후에, 입력 완료를 눌렀을 때 호출
+                        emitter?.onNext(query)
+                        return false
+                    }
 
-                override fun onQueryTextChange(newText: String): Boolean {
-                    // text가 변경될 때마다 호출
-                    return false
-                }
-            })
+                    override fun onQueryTextChange(newText: String): Boolean {
+                        // text가 변경될 때마다 호출
+                        binding.viewPager.setCurrentItem(0, true)
+                        emitter?.onNext(newText)
+                        return false
+                    }
+                })
+            }
         }
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                searchFragment.searchKeyword(it)
+            }
 
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
 }
